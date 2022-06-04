626a6d719722ffb2ec5bee9d3a222758ee742b45
juhyun1011 <juhyun4567@gmail.com>
2022-06-05 00:13:14 +0900

    Feat : Mypage 게시글 불러오기
    진행 중

--- post/views.py
+++ post/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render, redirect
+from .models import PostModel
 from django.http import HttpResponse
 
 def show_list(request):
     if request.method == 'GET':
-        return render(request,'post/mypage.html')
+        user = request.user.is_authenticated   # 사용자가 로그인이 되어 있는지 확인하기
+        if user:
+            all_post = PostModel.objects.all(author=user).order_by('-created_at')
+            return render(request,'post/mypage.html', {'posts': all_post})
+        else:  # 로그인이 되어 있지 않다면
+            return redirect('/sign-in/')
+
+
+
+
