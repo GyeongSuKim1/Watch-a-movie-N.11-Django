55ac4755b53aa0995e8f4d7bfba42e0bbd5a238b
nicesugi <104303285+nicesugi@users.noreply.github.com>
2022-06-08 00:11:28 +0900

    Feat : 상세페이지 추천영화 보여주고, 클릭시 상세페이지로 이동

--- movie/views.py
+++ movie/views.py
@@ -6,9 +6,9 @@ def home(request):
     if request.method == 'GET':
         movies = Movie.objects.all()
         for movie in movies:
-            print(movie)
             print(f'무비들 ->{movie.tag.all()}')
             movie.tags = list(movie.tag.all())
+
         return render(request, 'movie/home.html', {'movies' : movies})
 
     elif request.method == 'POST':
@@ -21,11 +21,19 @@ def detail(request, id):
         selected_movie = Movie.objects.get(id=id)
         print(selected_movie)
 
-
+        recommend = Movie.objects.all()
+        for movie in recommend:
+            movie.tags = list(movie.tag.all())
+            print(f'추천영화 리스트입니당->>>>{movie}')
 
         content = {
-            'selected': selected_movie
+            'selected' : selected_movie,
+            'movies' : movie,
+            'recommend' : recommend,
         }
 
         return render(request, 'movie/detail.html', content)
 
+
+    elif request.method == 'POST':
+        return render(request, 'movie/detail.html')
