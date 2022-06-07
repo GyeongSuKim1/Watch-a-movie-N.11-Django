ac3540aedfd4c88473eb156970773ea894f319d5
nicesugi <104303285+nicesugi@users.noreply.github.com>
2022-06-07 16:05:09 +0900

    Add : model 생성 - Tag / Movie / Machine

--- movie/models.py
+++ movie/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from user.models import UserModel
 
-# Create your models here.
+
+
+class Tag(models.Model):
+    class Meta:
+        db_table = "tag"
+
+    tag = models.CharField(max_length=256, default='')
+
+    def __str__(self):
+        return self.tag
+
+
+
+class Movie(models.Model):
+    class Meta:
+        db_table = "movie"
+
+    title = models.CharField(max_length=256, default='')
+    image = models.URLField(max_length=256)
+    score = models.DecimalField(max_digits=2, decimal_places=1)
+    desc = models.TextField()
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+
+
+class machine(models.Model):
+    class Meta:
+        db_table = "machine"
+
+    user_id = models.ForeignKey(UserModel, on_delete=models.CASCADE)
+    movie_id = models.ManyToManyField(Movie)
