dae7eaa48f4797e9ba977d2089765f68d5bdfe10
nicesugi <104303285+nicesugi@users.noreply.github.com>
2022-06-08 15:38:07 +0900

    Feat : 영화 랜덤으로 보여주기 / 선택영화가 추천영화랑 같을 경우 제외하고 보여주기

--- movie/views.py
+++ movie/views.py
@@ -1,13 +1,14 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 from movie.models import Movie
 
 
 def home(request):
     if request.method == 'GET':
-        movies = Movie.objects.all()
+        movies = Movie.objects.order_by('?') # 랜덤으로 보여주기
+
         for movie in movies:
             print(f'무비들 ->{movie.tag.all()}')
-            movie.tags = list(movie.tag.all())
+            movie.tags = list(movie.tag.all()) # 태그 넣어주기
 
         return render(request, 'movie/home.html', {'movies' : movies})
 
@@ -19,20 +20,26 @@ def home(request):
 def detail(request, id):
     if request.method == 'GET':
         selected_movie = Movie.objects.get(id=id)
-        print(selected_movie)
+        print(f'선택한 영화 : {selected_movie}')
+        recommend = Movie.objects.order_by('?')
 
-        recommend = Movie.objects.all()
-        for movie in recommend:
-            movie.tags = list(movie.tag.all())
-            print(f'추천영화 리스트입니당->>>>{movie}')
+        if selected_movie in recommend:
+            print('오우!!')
+            recommend = Movie.objects.exclude(id=selected_movie.id)
+            print(f'선택영화와 추천영화가 같다면 제외하고 보여주기!{recommend}')
 
-        content = {
-            'selected' : selected_movie,
-            'movies' : movie,
-            'recommend' : recommend,
-        }
+            for movie in recommend:
+                movie.tags = list(movie.tag.all())
+                print(f'추천영화 리스트 들!->{movie}')
 
-        return render(request, 'movie/detail.html', content)
+            content = {
+                'selected' : selected_movie,
+                'movies' : movie,
+                'recommend' : recommend,
+            }
+            return render(request, 'movie/detail.html', content)
+        else:
+            return render(request, 'movie/detail.html')
 
 
     elif request.method == 'POST':
