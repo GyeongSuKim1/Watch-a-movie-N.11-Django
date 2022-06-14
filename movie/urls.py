e1c0ac5e93a5093ad3621c44825504c60070533c
Daegeun Jeong <wjdeorms27@naver.com>
2022-06-15 00:45:22 +0900

    Feat : url 수정

--- movie/urls.py
+++ movie/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('detail/<int:id>/', views.detail, name='detail'),
     path('search/', views.search, name='search'),
-    path('<str:name>/', views.tagging, name='tagging'),
+    path('tag/<str:name>/', views.tagging, name='tagging'),
 ]
