0c6123f321d4cf156b0874c3011dc9ccf37c8cae
Daegeun Jeong <99387514+nikevapormax@users.noreply.github.com>
2022-08-18 14:17:04 +0900

    Update : 주석 정리

--- user/models.py
+++ user/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User, AbstractUser
 
 
-# Create your models here.
 class UserModel(AbstractUser):
 	class Meta:
 		db_table = 'user'
