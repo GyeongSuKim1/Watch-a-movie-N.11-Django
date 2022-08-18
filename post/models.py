9ddae0b02304c9a4a6cf824d617846dd82f1c392
Daegeun Jeong <99387514+nikevapormax@users.noreply.github.com>
2022-08-18 14:11:24 +0900

    Update : 주석 정리

--- post/models.py
+++ post/models.py
@@ -10,6 +10,6 @@ class PostModel(models.Model):
 
     author = models.ForeignKey(UserModel, on_delete=models.CASCADE)
     title = models.ForeignKey(Movie, on_delete=models.CASCADE)
-    score = models.DecimalField(max_digits=2, decimal_places=1) #소수점으로 데이터 지정
+    score = models.DecimalField(max_digits=2, decimal_places=1)
     content = models.TextField(max_length=1000)
     created_at = models.DateTimeField(auto_now_add=True)
